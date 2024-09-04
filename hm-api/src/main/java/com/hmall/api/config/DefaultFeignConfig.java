77c6e9f6fa53324e94c1f55be6fdbe36b59c2537
曾繁曦 <2628690042@qq.com>
2024-09-04 11:06:33 +0800

    update

---
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -0,0 +1,21 @@
+package com.hmall.api.config;
+
+import com.hmall.common.utils.UserContext;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+
+public class DefaultFeignConfig {
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                Long userId = UserContext.getUser();
+                if (userId != null) {
+                    template.header("user-info", userId.toString());
+                }
+            }
+        };
+    }
+}
--- hm-gateway/src/test/java/com/hmall/gateway/GatewayApplicationTests.java
+++ hm-gateway/src/test/java/com/hmall/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hmall.hmgateway;
+package com.hmall.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HmGatewayApplicationTests {
+class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
